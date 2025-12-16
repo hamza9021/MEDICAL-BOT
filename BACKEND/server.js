819c41658a5ada18918d4f910982c50deae8e7bc
Hamza Riaz <hriaz9803@gmail.com>
2025-12-16 15:05:10 -0800

    Set trust proxy for Express app

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -8,6 +8,8 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+app.set("trust proxy", 1);
 // Middleware
 app.use(cors({
   origin: process.env.CORS_ORIGIN || "*",
